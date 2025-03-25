1063bc4de2930811c9852e9e94167b1da4e93d47
nomikatsuaki <nkxyo03@gmail.com>
2025-03-25 20:48:37 +0900

    ecsite

---
+++ src/test/java/jp/cp/internous/ecsite/EcsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cp.internous.ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
